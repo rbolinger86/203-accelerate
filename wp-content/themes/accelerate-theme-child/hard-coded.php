31d62e1ed0c06bff67a704090e8ddc6f2e22dbec
Robert Bolinger <rbolinger86@gmail.com>
2018-09-27 12:00:46 -0500

    git commit . 	../../../wp-admin/js/code-editor.min.js C# ../../../wp-includes/customize/class-wp-customize-code-editor-control.php ../../../wp-includes/js/mediaelement/mediaelementplayer-legacy.css git push origin 203-accelerate
    ^X
    XXXXX
    
    X
    X

---
+++ wp-content/themes/accelerate-theme-child/hard-coded.php
@@ -0,0 +1,7 @@
+<article class="case-study"><aside class="case-study-sidebar">
+<h2>MailChimp Campaign</h2>
+<h4>Content Strategy, Design &amp; Development</h4>
+<h4>Client: MailChimp</h4>
+This is a project I made for this company for this.  Here is the early stage startup that I helped design and do front-end and back-end dev for.
+<p class="read-more-link"><a href="#">Visit Live Site ›</a></p>
+</aside></article>
